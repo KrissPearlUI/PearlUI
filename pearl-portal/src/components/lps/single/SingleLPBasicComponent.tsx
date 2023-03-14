eb11892d1aba6f7f9c92ec5a5a6fa5b7a139bbbc
Kristina Nikolova <kni@EMERALD.local>
2023-03-14 16:57:55 +0100

    added autocompletes

--- pearl-portal/src/components/lps/single/SingleLPBasicComponent.tsx
+++ pearl-portal/src/components/lps/single/SingleLPBasicComponent.tsx
@@ -1,6 +1,6 @@
-import {Grid,Paper,Typography} from '@mui/material';
+import {Autocomplete, AutocompleteRenderInputParams, Grid,Paper,TextField,Typography} from '@mui/material';
 import {darken, useTheme} from "@mui/material/styles";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { setTopBarTitle } from '../../../redux/slices/appSlice';
 import { useAppDispatch } from '../../../redux/store';
 import {Theme} from "@mui/material";
@@ -8,6 +8,35 @@ import {createStyles,makeStyles} from '@mui/styles';
 import LPChartComponent from '../../../components/landing/LPChart';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../redux/slices/rootSlice';
+import { Fund, LP, PCO } from '../../../models/lps/lpModels';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+const autocompleteInputStyles = makeStyles((theme: Theme) => ({
+    autocomplete: {
+        'borderRadius': 5,
+        'backgroundColor': 'transparent',
+        '& input::placeholder': {
+            color: theme.palette.text.primary
+        },
+        '& .Mui-disabled': {
+            color: theme.palette.text.primary,
+            opacity: 0.8
+        }
+    },
+    textInput: {
+        'color': theme.palette.text.primary,
+        'fontWeight': 800,
+        'fontFamily': 'Raleway',
+        /* 'height': '2.5em', */
+        'fontSize': 10,
+        '& .MuiIconButton-label': {
+            color: theme.palette.text.primary
+        }
+    },
+    clearIndicator: {
+        color: theme.palette.text.primary
+    }
+}));
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -20,15 +49,63 @@ const useStyles = makeStyles((theme: Theme) =>
         summary: {
             flex:1
         },
+        inputRoot: {
+            'borderRadius': 5,
+            'backgroundColor': 'transparent',
+    /*         '& .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'black'
+            },*/
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+                borderColor: theme.palette.primary.main
+            },
+          /*  '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                borderColor: 'black'
+            }, */
+            '& .MuiChip-root': {
+                color: theme.palette.text.primary,
+                backgroundColor: 'transparent',
+                borderRadius: 5
+            },
+            '& .MuiChip-deleteIconSmall': {
+                color: theme.palette.text.primary
+            }
+        },
+        option: {
+            'background': theme.palette.background.paper,
+            '&:hover': {
+                color: theme.palette.primary.main,
+                fontWeight: 400,
+                fontFamily: 'Raleway'
+            },
+            '&[aria-selected="true"]': {
+                background: theme.palette.background.paper,
+                color: theme.palette.primary.main,
+                fontWeight: 700,
+                fontFamily: 'Raleway'
+            }
+        },
+        popupIndicator: {
+            '&.MuiIconButton-root': {
+                color: theme.palette.text.primary
+            }
+        },
+        clearIndicator: {
+            color: theme.palette.text.primary
+        },
     }),
 );
 
 const SingleLPBasic = () => {
     const classes=useStyles();
     const theme=useTheme();
+    const autocompleteInputClasses=autocompleteInputStyles();
     const dispatch = useAppDispatch();
     const {selectedLP} = useSelector((state: RootState) => state.lps);
-    
+    const [selectedLPValue, setSelectedLPValue] = useState<any>(null);
+
+    const onLPChange = (event: any) => {
+        setSelectedLPValue(event);
+    };
     return (
         <Grid container spacing={2} sx={{display:'flex',flex:1, justifyContent:'flex-start', alignItems:'flex-start', flexDirection:'row',paddingRight:'1em'}}>
             <Grid item xs={12}>
@@ -80,12 +157,58 @@ const SingleLPBasic = () => {
                 <Grid container spacing={1} sx={{display:'flex', justifyContent:'space-between', alignItems:'flex-start', flexDirection:'row'}}>
                         <Grid container spacing={1} item xs={4} sx={{display:'flex',  flexDirection:'column'}}>
                             <Grid item sx={{display:'flex'}}>
-                            <Typography sx={{color:theme.palette.secondary.main, marginRight:'0.5em', fontWeight:400}}>Domicile:</Typography>
-                            <Typography sx={{color:theme.palette.text.primary, fontWeight:500}}>{selectedLP?.country}</Typography>
+                            <Autocomplete
+                                id={'lpdAutocomplete'}
+                                popupIcon={<ExpandMoreIcon/>}
+                                size={'small'}
+                                autoHighlight={true}
+                                autoSelect={true}
+                                autoComplete={false}
+                                disableClearable
+                                classes={classes}
+                                sx={{marginRight:'1em', width:'320px'}}
+                                isOptionEqualToValue={(option, value) => option === value}
+                                onChange={(e, value: Fund) => onLPChange(value)}
+                                value={selectedLPValue??undefined}
+                                options={selectedLP?.funds ?? []}
+                                getOptionLabel={(option: Fund) => option ? option.fundName : ''}
+                                renderInput={(params: AutocompleteRenderInputParams) => {
+                                    params.InputProps.className = autocompleteInputClasses.textInput;
+                                    return <TextField {...params} 
+                                    className={autocompleteInputClasses.autocomplete}
+                                                    variant="standard" 
+                                                    autoComplete="off"
+                                                    type={'text'}
+                                                    label='Select a LP'/>;
+                                }}
+                                />
                             </Grid>
                             <Grid item sx={{display:'flex'}}>
-                            <Typography sx={{color:theme.palette.secondary.main, marginRight:'0.5em',fontWeight:400}}>Address:</Typography>
-                            <Typography sx={{color:theme.palette.text.primary,fontWeight:500}}>{selectedLP?.country}</Typography>
+                            <Autocomplete
+                                id={'lpdAutocomplete'}
+                                popupIcon={<ExpandMoreIcon/>}
+                                size={'small'}
+                                autoHighlight={true}
+                                autoSelect={true}
+                                autoComplete={false}
+                                disableClearable
+                                classes={classes}
+                                sx={{marginRight:'1em', width:'320px'}}
+                                isOptionEqualToValue={(option, value) => option === value}
+                                onChange={(e, value: Fund) => onLPChange(value)}
+                                value={selectedLPValue??undefined}
+                                options={selectedLP?.funds ?? []}
+                                getOptionLabel={(option: Fund) => option ? option.fundName : ''}
+                                renderInput={(params: AutocompleteRenderInputParams) => {
+                                    params.InputProps.className = autocompleteInputClasses.textInput;
+                                    return <TextField {...params} 
+                                    className={autocompleteInputClasses.autocomplete}
+                                                    variant="standard" 
+                                                    autoComplete="off"
+                                                    type={'text'}
+                                                    label='Select a LP'/>;
+                                }}
+                                />
                             </Grid>
                         </Grid>
                         <Grid container spacing={1} item xs={4} sx={{display:'flex',  flexDirection:'column'}}>
@@ -96,12 +219,58 @@ const SingleLPBasic = () => {
                         </Grid>
                         <Grid container spacing={1} item xs={4} sx={{display:'flex',  flexDirection:'column'}}>
                         <Grid item sx={{display:'flex'}}>
-                            <Typography sx={{color:theme.palette.secondary.main, marginRight:'0.5em', fontWeight:400}}>Main Contact:</Typography>
-                            <Typography sx={{color:theme.palette.text.primary, fontWeight:500}}>{selectedLP?.country}</Typography>
+                        <Autocomplete
+                                id={'lpdAutocomplete'}
+                                popupIcon={<ExpandMoreIcon/>}
+                                size={'small'}
+                                autoHighlight={true}
+                                autoSelect={true}
+                                autoComplete={false}
+                                disableClearable
+                                classes={classes}
+                                sx={{marginRight:'1em', width:'320px'}}
+                                isOptionEqualToValue={(option, value) => option === value}
+                                onChange={(e, value: PCO) => onLPChange(value)}
+                                value={selectedLPValue??undefined}
+                                options={selectedLP?.pcos ?? []}
+                                getOptionLabel={(option: PCO) => option ? option.shortName : ''}
+                                renderInput={(params: AutocompleteRenderInputParams) => {
+                                    params.InputProps.className = autocompleteInputClasses.textInput;
+                                    return <TextField {...params} 
+                                    className={autocompleteInputClasses.autocomplete}
+                                                    variant="standard" 
+                                                    autoComplete="off"
+                                                    type={'text'}
+                                                    label='Select a LP'/>;
+                                }}
+                                />
                             </Grid>
                             <Grid item sx={{display:'flex'}}>
-                            <Typography sx={{color:theme.palette.secondary.main, marginRight:'0.5em', fontWeight:400}}>Relationship Partner:</Typography>
-                            <Typography sx={{color:theme.palette.text.primary, fontWeight:500}}>{selectedLP?.country}</Typography>
+                            <Autocomplete
+                                id={'lpdAutocomplete'}
+                                popupIcon={<ExpandMoreIcon/>}
+                                size={'small'}
+                                autoHighlight={true}
+                                autoSelect={true}
+                                autoComplete={false}
+                                disableClearable
+                                classes={classes}
+                                sx={{marginRight:'1em', width:'320px'}}
+                                isOptionEqualToValue={(option, value) => option === value}
+                                onChange={(e, value: PCO) => onLPChange(value)}
+                                value={selectedLPValue??undefined}
+                                options={selectedLP?.pcos ?? []}
+                                getOptionLabel={(option: PCO) => option ? option.shortName : ''}
+                                renderInput={(params: AutocompleteRenderInputParams) => {
+                                    params.InputProps.className = autocompleteInputClasses.textInput;
+                                    return <TextField {...params} 
+                                    className={autocompleteInputClasses.autocomplete}
+                                                    variant="standard" 
+                                                    autoComplete="off"
+                                                    type={'text'}
+                                                    label='Select a LP'/>;
+                                }}
+                                />
                             </Grid>
                         </Grid>
                     </Grid>
